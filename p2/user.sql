2c70e4b8641397302e63026baf4b5bb968b4c1f6
Ryan Sun <67710212+Nofachad@users.noreply.github.com>
2023-07-20 14:30:38 -0700

    Add files via upload

---
+++ p2/user.sql
@@ -0,0 +1,75 @@
+CREATE TABLE IF NOT EXISTS `user` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `email` varchar(255),
+	`username` varchar(10),
+	`password` varchar(64),
+	PRIMARY KEY (`id`)
+);
+	
+-- INSERT INTO player
+-- 	(`username`, `password`)
+-- VALUES
+--     ('Parthenia', '$2y$10$Uc2MGBbfLE12WgRLMLp2iunQoMn1USVmd5rOxkzKyDYtJQWv4W8.a'),
+-- 	('Tomika', '$2y$10$/l9SUQqYDTz0AYY4xLZbGepLbwWbl8tP62pJRQFRbcvlCuJXsTzqa'),
+-- 	('Kip', '$2y$10$OxEYGhKdsmnsQScu9wcn4ONlYDX5EwuPTvoZjkfd0etQ4YlbylyKC'),
+-- 	('Imelda', '$2y$10$4PIdQRGlLDz78fKDtpHDEeisiwmOdbtJn4JnuQ7nZVZ.e8Heg/FA6'),
+-- 	('Geoffrey', '$2y$10$Xu51JzZf9TIRCcIg26hrL.tmg1pxvXf7ZnQaS1Ys4Jv402p8JBJkq'),
+-- 	('Shanel', '$2y$10$EWRPL7frameMLpLf7VqLIO6uQv5d0Ce6y3ARDNBFLuhB1iBXVZREe'),
+-- 	('Lilliana', '$2y$10$rDF0WlFo8IP3Ceu5i1maaOA98.9mkDjvPi0W9Q6fJSuGA4SKLx2e6'),
+-- 	('Deloris', '$2y$10$4sxlyUjxzpLmqgD18l42bOCZwwesvCfXSv6zgzB.vL6wmsMS3ptV.'),
+-- 	('Giovanna', '$2y$10$pzSBWacaqvN4YYnat6urbuLqC1dtqzveFXismnDs5HKviDPY4LHTW'),
+-- 	('Suanne', '$2y$10$IZ2nVrgzgOHYN6Kalg6RlubMITjXQZDIMVz1mLAA8tPcVSYUuapY.');
+
+CREATE TABLE IF NOT EXISTS `saves` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `user_id` varchar(255),
+	`saveNum` int,
+	PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `unlocked` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `user_id` varchar(255),
+	`saveNum` int,
+	PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `lvls` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `user_id` varchar(255),
+	`saveNum` int,
+	PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `scenes` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `user_id` varchar(255),
+	`saveNum` int,
+	PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `objects` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `user_id` varchar(255),
+	`saveNum` int,
+	PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `permaObjects` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `user_id` varchar(255),
+	`saveNum` int,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `contains` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `user_id` varchar(255),
+	`saveNum` int,
+	PRIMARY KEY (`id`)
+);
